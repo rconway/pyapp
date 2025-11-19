c7ade2745587d3094db47051306a3d2cbd84327c
Richard Conway <richard@rconway.co.uk>
2025-11-19 16:21:09 +0000

    Run specific tagged container version

--- container/run.sh
+++ container/run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-"$(dirname "$0")"/build.sh
+if [ -n "$1" ]; then
+  TAG=":$1"
+else
+  "$(dirname "$0")"/build.sh
+fi
 
-docker run --rm -p 8000:8000 -v $PWD/.env:/app/.env ghcr.io/rconway/authapp
+docker run --rm -p 8000:8000 -v $PWD/.env:/app/.env "ghcr.io/rconway/authapp${TAG}"
